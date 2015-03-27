f38f278b9a0fb8ee0373bd4ec8a43959e1442a8a
Jay Wengrow <jaywngrw@gmail.com>
2015-03-26 20:04:01 -0500

    ng-click

--- app/assets/javascripts/page_ctrl.js
+++ app/assets/javascripts/page_ctrl.js
@@ -6,6 +6,13 @@
     $scope.purpleHippo = "Hello!";
     $scope.greenHippo = "Hippo";
     
+    $scope.reviews = ["AngularJS is the best thing since sliced bread!", "I love AngularJS, especially when I'm programming!", "AngularJS is waaaaay better than Chinese Checkers."];
+
+    $scope.addReview = function(review) {
+      $scope.reviews.push(review);
+      
+    }
+
     window.scope = $scope;
   });
 })();
