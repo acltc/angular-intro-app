04e73cc56e0ea88519c7472c15e6f541c6bf5204
Jay Wengrow <jaywngrw@gmail.com>
2015-03-29 10:54:24 -0500

    ng-if and objects

--- app/assets/javascripts/page_ctrl.js
+++ app/assets/javascripts/page_ctrl.js
@@ -6,11 +6,35 @@
     $scope.purpleHippo = "Hello!";
     $scope.greenHippo = "Hippo";
     
-    $scope.reviews = ["AngularJS is the best thing since sliced bread!", "I love AngularJS, especially when I'm programming!", "AngularJS is waaaaay better than Chinese Checkers."];
+    $scope.reviews = [
+      {
+        text: "AngularJS is the best thing since sliced bread!",
+        stars: 5,
+        reviewer: "Emily Jackson"
+      },
+      {
+        text: "I love AngularJS, especially when I'm programming!",
+        stars: 4,
+        reviewer: "Max Burlington"
+      },
+      {
+        text: "AngularJS is waaaaay better than Chinese Checkers.",
+        stars: 3,
+        reviewer: "Ursula Watson"
+      }
+    ];
 
-    $scope.addReview = function(review) {
+    $scope.addReview = function(newName, newStars, newText) {
+      var review = {
+        reviewer: newName,
+        stars: newStars,
+        text: newText
+      };
       $scope.reviews.push(review);
-      
+    }
+
+    $scope.isPositive = function(review) {
+      return review.indexOf("bad") === -1
     }
 
     window.scope = $scope;
